1af822b4e510b6e16dedffec6c1e76e5a73bdff8
Ulas Tan Ersoyak <ulassztanersoyak@gmail.com>
2026-08-30 23:50:02 +0200

    neovim transparency

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,27 +1,28 @@
 -- Colorscheme + statusline
 return {
-  {
-    "sainnhe/gruvbox-material",
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "medium" -- "hard" | "medium" | "soft"
-      vim.g.gruvbox_material_better_performance = 1
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        theme = "gruvbox-material",
-        component_separators = "",
-        section_separators = "",
-      },
-      sections = {
-        lualine_c = { { "filename", path = 1 } }, -- relative path
-        lualine_x = { "diagnostics", "lsp_status", "filetype" },
-      },
-    },
-  },
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "medium" -- "hard" | "medium" | "soft"
+			vim.g.gruvbox_material_transparent_background = 2
+			vim.g.gruvbox_material_better_performance = 1
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				theme = "gruvbox-material",
+				component_separators = "",
+				section_separators = "",
+			},
+			sections = {
+				lualine_c = { { "filename", path = 1 } }, -- relative path
+				lualine_x = { "diagnostics", "lsp_status", "filetype" },
+			},
+		},
+	},
 }
